a856eafd6a9be59194d7390e7d20e8a8b1b022dc
lucas-paz <lucas.hpaz@outlook.com>
2021-10-01 18:59:18 -0300

    footer

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './styles.css';
+
+function Footer() {
+  return (
+    <div id="footer">
+      <h3>Lucas Henrique Paz</h3>
+      <span>
+        Follow me to see more of my work on my social medias:
+      </span>
+      <div>
+        <a href="https://github.com/LucasH-Paz" target="_blank" rel="noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/lucas-paz-dev/" target="_blank" rel="noreferrer">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="https://www.instagram.com/lucasofpeace/" target="_blank" rel="noreferrer">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
+      <span className="copyright">Copyright© 2021 Lucas Paz. All rights reserved</span>
+    </div>
+  )
+}
+
+export default Footer;
